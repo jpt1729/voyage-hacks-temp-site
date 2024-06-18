141ece9749616315d52e90048f48e2335e60e4de
John <136768731+jpt1729@users.noreply.github.com>
2024-06-18 10:53:33 -0500

    making it look nicer!

---
+++ src/components/Newsletter/index.js
@@ -0,0 +1,8 @@
+"use client"
+export default function NewsletterForm(){
+    return (
+        <form>
+          <input type='email' placeholder="Join our newsletter!"/><input type='submit' value='Sign up'/>
+        </form>
+    )
+}
